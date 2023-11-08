f14b65f203f849154fbce213bdd288404299f628
mzztin <exze.real@gmail.com>
2023-11-08 13:35:20 +0100

    feat(level 1 & 2 & 3): templates eingefügt

--- scripts/level/level_3.gd
+++ scripts/level/level_3.gd
@@ -1,1 +1,12 @@
 extends Node2D
+
+@onready var player = $Player
+@onready var fog = $Fog
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	fog.draw()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	fog.tick(player.position)
