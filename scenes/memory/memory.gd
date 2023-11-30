928804cf3a030ce8e581dc1aa40d8fb8075ba1a2
Benedikt <148131976+Benedikt52@users.noreply.github.com>
2023-12-01 00:19:05 +0100

    docs(memory): detailed comments added

--- scenes/memory/memory.gd
+++ scenes/memory/memory.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
-# VARIABLEN HIER ANPASSEN
+# VARIABLEN HIER ANPASSEN UND ENTSPRECHENDE METHODEN UNTEN AUSKOMMENTIEREN
 var memory_scene_path = "res://scenes/memory/memory.tscn"
 var next_scene_path = "res://path/to/your_next_scene.tscn"
 var max_game_moves = 50
 
+# lade memory karten assets
 var card = preload("res://scenes/memory/card.tscn")
 var Textures = [
 	preload("res://assets/memory/memory_card0.png"),
@@ -24,6 +25,7 @@ var ROW = 4
 var COL = 5
 var PairsToBeFound = (ROW * COL) / 2
 
+# HUD variablen
 var gameMoves = 0
 var gameMovesLabel
 
@@ -31,6 +33,7 @@ func _ready():
 	setupHUD()
 	var cards_container = get_node("CanvasLayer/Panel/cards") # pfad zum sprite an dem die karten spawnen
 	
+	# instanziiere memory karten
 	for t in Textures:
 		var newCard = card.instantiate()
 		newCard.get_node("obverse").texture = t
@@ -39,21 +42,25 @@ func _ready():
 		newCard_match.get_node("obverse").texture = t
 		cards.append(newCard_match)
 		
-	cards.shuffle()
+	cards.shuffle() # mische karten
 		
+	# erzeuge memory karten am 'cards' sprite
 	for row in ROW:
 		for col in COL:
 			var c = cards[col + row * 5]
 			c.position = Vector2(125 * col, 125 * row)
 			cards_container.add_child(c)
 
+# instanziiert/aktualisiert HUD
 func setupHUD():
 	gameMovesLabel = get_node("CanvasLayer/Panel/PanelContainerTop/VBoxContainer/HBoxContainer/counter")
 	gameMovesLabel.text = str(gameMoves)
 
+# prüft memory karten
 func check():
 	print(open_cards)
 	
+	# wenn zwei karten gewählt wurden
 	if len(open_cards) >= 2:
 		increase_move_count()
 		for card in cards:
@@ -75,30 +82,36 @@ func check():
 			print("turnback timer start")
 			$TurnBackTimer.start()
 
+# clear array
 func continue_control():
 	for card in cards:
 		card.can_control = true
 	open_cards = []
-	
+
+# timer: wenn karten ein match waren -> karten werden gelöscht
 func _on_delete_timer_timeout():
 	for card in open_cards:
 		card.queue_free()
 		cards.erase(card)
 	continue_control()
 
+# timer: karten werden wieder umgedreht
 func _on_turn_back_timer_timeout():
 	for card in open_cards:
 		card.get_node("AnimationPlayer").play("turn_back")
 	continue_control()
-	
+
+# timer: wenn spiel gelöst -> scenen wechsel
 func _on_scene_change_timer_timeout():
 	# hier kann eine neue scene geladen werden 
 	# zum beispiel: get_tree().change_scene(next_scene_path)
 	print("Alle Paare gefunden! Scene wechsel.")
-	
+
+# funktion für timer start nach ende
 func on_all_pairs_found():
 	$SceneChangeTimer.start()
 
+# funktion fängt <q> tastendruck ab -> scenen wechsel
 var is_scene_changing = false
 func _process(delta):
 	if Input.is_key_pressed(KEY_Q) and not is_scene_changing:
@@ -107,21 +120,26 @@ func _process(delta):
 		# taste q gedrückt -> wechsel scene
 		# zum beispiel: get_tree().change_scene(next_scene_path)
 
+# button: fängt tastendruck im interface ab
 func _on_button_pressed():
 	print("q") 
 	# button q gedrückt -> wechsel scene
 	# zum beispiel: get_tree().change_scene(next_scene_path)
-	
+
+# inkrementiert 'gameMoves', aktualiert HUD 
+# und prüft ob 'max_game_moves' erreicht -> neue memory scene wird geladen
 func increase_move_count():
 	gameMoves += 1
 	setupHUD()
 	if gameMoves >= max_game_moves:
 		load_new_memory_game()
 
+# lädt neue memory scene
 func load_new_memory_game():
 	get_tree().reload_current_scene()
 	print("new scene")
-	
+
+# ruft check() methode auf
 func card_selected(card):
 	open_cards.append(card)
 	check()
