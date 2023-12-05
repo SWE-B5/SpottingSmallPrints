9cec0aa5cd42cd52509035a3e38d4a3c538f406c
martin <exze.real@gmail.com>
2023-12-05 17:47:21 +0100

    feat(lights_out): final touches

--- scripts/games/lights_out_game.gd
+++ scripts/games/lights_out_game.gd
@@ -1,16 +1,16 @@
 extends Node2D
 class_name LightsOutGame
 
-signal finish()
+@onready var plates = [$"0", $"1", $"2", $"3", $"4", $"5", $"6", $"7", $"8"]
 
-func _process(delta):
-	print(is_finished())
+signal finish()
 
 func is_finished():
 	var finished = true
-	for plate in [$"0", $"1", $"2", $"3", $"4", $"5", $"6", $"7", $"8"]:
+	for plate in plates:
 		if plate.current_mode != PressurePlateLO.Mode.ACTIVATED:
 			finished = false
+	print(finished)
 	return finished
 
 func block_all_changes():
@@ -22,53 +22,53 @@ func check_game():
 		block_all_changes()
 		finish.emit()
 
-func _on__mode_manually_changed_0(new_mode):
+func _on__change_mode0(new_mode):
 	$"1".toggle()
 	$"3".toggle()
 	check_game()
 
-func _on__mode_manually_changed_1(new_mode):
+func _on__change_mode1(new_mode):
 	$"0".toggle()
 	$"2".toggle()
 	$"4".toggle()
 	check_game()
 	
-func _on__mode_manually_changed_2(new_mode):
+func _on__change_mode2(new_mode):
 	$"1".toggle()
 	$"5".toggle()
 	check_game()
 	
-func _on__mode_manually_changed_3(new_mode):
+func _on__change_mode3(new_mode):
 	$"0".toggle()
 	$"4".toggle()
 	$"6".toggle()
 	check_game()
 	
-func _on__mode_manually_changed_4(new_mode):
+func _on__change_mode4(new_mode):
 	$"1".toggle()
 	$"3".toggle()
 	$"5".toggle()
 	$"7".toggle()
 	check_game()
 	
-func _on__mode_manually_changed_5(new_mode):
+func _on__change_mode5(new_mode):
 	$"2".toggle()
 	$"4".toggle()
 	$"8".toggle()
 	check_game()
 	
-func _on__mode_manually_changed_6(new_mode):
+func _on__change_mode6(new_mode):
 	$"3".toggle()
 	$"7".toggle()
 	check_game()
 	
-func _on__mode_manually_changed_7(new_mode):
+func _on__change_mode7(new_mode):
 	$"4".toggle()
 	$"6".toggle()
 	$"8".toggle()
 	check_game()
 	
-func _on__mode_manually_changed_8(new_mode):
+func _on__change_mode8(new_mode):
 	$"5".toggle()
 	$"7".toggle()
 	check_game()
