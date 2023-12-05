74fb3dfc0f61e17ddec47be0daffa0badd6db918
Girskorr <58144758+Girskorr@users.noreply.github.com>
2023-12-05 14:35:12 +0100

    feat: Skull Hitbox hinzugefuegt

--- scripts/enemies/skull.gd
+++ scripts/enemies/skull.gd
@@ -2,12 +2,15 @@ extends CharacterBody2D
 
 
 @export var speed: float = 100
+@export var markerid : String
+@onready var sprite = $sprite
 var destination_points: Array = []  # Array to store Marker2D points
 var current_destination: Vector2  # Current destination point
 
 func _ready():
+	sprite.play()
 	# Populate the array with Marker2D points
-	for marker in get_tree().get_nodes_in_group("marker"):
+	for marker in get_tree().get_nodes_in_group(markerid):
 		if marker is Marker2D:
 			destination_points.append(marker.global_position)
 
@@ -26,3 +29,10 @@ func _process(delta):
 func choose_random_destination():
 	# Choose a random destination from the array
 	current_destination = destination_points[randi() % destination_points.size()]
+
+
+func _on_hitbox_area_entered(area):
+	if area.is_in_group("hitbox_player"):
+		choose_random_destination()
+		# Hier werden die Leben abgezogen und Immunity frames berechnet
+		print("Damage dealt to player:")
