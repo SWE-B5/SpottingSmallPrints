faea6761099f4e40b1ec92c0cf06e93ea4263de8
Philogex <64011915+Philogex@users.noreply.github.com>
2023-12-07 19:39:09 +0100

    included health functions

--- scripts/traps/spikes.gd
+++ scripts/traps/spikes.gd
@@ -5,5 +5,5 @@ func _ready():
 
 func _on_body_entered(body):
 	if(PlayerVariables.immunity_frames <= 0):
-		#Health.damage_player()
+		Health.damage_player()
 		PlayerVariables.immunity_frames = PlayerVariables.immunity_duration
