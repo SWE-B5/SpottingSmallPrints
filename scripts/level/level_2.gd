60cc2e2a148ef4995ecf5ffa7fd472c92bb15a2b
Til <107125576+1Shark2Sharks@users.noreply.github.com>
2023-12-14 15:36:50 +0100

    feat(level_2): Gegner hinzugefügt

--- scripts/level/level_2.gd
+++ scripts/level/level_2.gd
@@ -5,7 +5,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	PlayerVariables.load_easy_game()
+	PlayerVariables.initialize_new_game(PlayerVariables.Difficulty.MEDIUM)
 	fog.init($TileMap, $Player)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
