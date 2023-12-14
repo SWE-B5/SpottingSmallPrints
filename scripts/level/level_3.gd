1e645409ed4707204d14e97c3f4be6d97fb7238d
Philogex <64011915+Philogex@users.noreply.github.com>
2023-12-14 20:15:49 +0100

    fixed incoming pulls

--- scripts/level/level_3.gd
+++ scripts/level/level_3.gd
@@ -4,11 +4,10 @@ extends Node2D
 @onready var fog = $Fog
 @onready var tilemap = $TileMap
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	fog.init(tilemap, player)
-	Health.reset_health() #muss bleiben
-	Inventory.update_after_death() #muss bleiben
+	Health.reset_health()
+	Inventory.update_after_death()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
