6c6b90b50872e1cb009305fd6c0385d49bb72881
Girskorr <58144758+Girskorr@users.noreply.github.com>
2023-12-19 20:28:18 +0100

    script erweitert

--- scripts/items/diamond_truhe.gd
+++ scripts/items/diamond_truhe.gd
@@ -1,1 +1,37 @@
-extends Node
+
+extends StaticBody2D
+
+#Speichert die ID des Schluessel der gedropped wird
+var schlüssel_id = 0
+
+@onready var interaction_area: InteractionArea = $InteractionArea
+
+@onready var sprite = $AnimatedSprite2D
+
+@onready var collisionshape = $CollisionShape2D
+
+@onready var detecion_area = $InteractionArea/DetectionArea
+
+@onready var is_closed = true
+
+
+
+
+func _ready():
+	interaction_area.interact = Callable(self, "_on_interact")
+	sprite.animation =  "closed"
+
+func _on_interact():
+	var resource = load("res://dialogs/note_dialog.dialogue")
+	if is_closed:
+		Inventory.collect_item(Inventory.Item_Type.DIAMOND, schlüssel_id)
+		#Hier muss noch der Dialog abgespielt werden
+		is_closed = false
+		# Interaktion mit Truhe deaktivieren
+		detecion_area.disabled = true
+		# Textur der offenen Truhe laden 
+		sprite.animation =  "open"
+		DialogManager.show_dialogue_balloon(resource,"Diamantene_Kiste")
+		
+		
+		
