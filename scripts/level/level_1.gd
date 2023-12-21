6581163424da743af55427fe4b4749a0a601905e
Benedikt <148131976+Benedikt52@users.noreply.github.com>
2023-12-21 20:37:15 +0100

    added new skeletons

--- scripts/level/level_1.gd
+++ scripts/level/level_1.gd
@@ -5,12 +5,8 @@ extends Node2D
 @onready var tilemap = $TileMap
 
 func _ready():
-	PlayerVariables.initialize_new_game(PlayerVariables.Difficulty.EASY)
-	fog.init($TileMap, $Player)
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	fog.update_pos(player.position)
+	PlayerVariables.initialize_new_game(PlayerVariables.Difficulty.EASY) # kann später raus
 	fog.init(tilemap, player)
 	Health.reset_health() #muss drin bleiben!
 	Inventory.update_after_death() #muss drin bleiben!
+	
