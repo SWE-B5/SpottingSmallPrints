547e7c1bca8694472d09bbb6917bfc0a668cd43d
Philogex <64011915+Philogex@users.noreply.github.com>
2023-12-22 22:07:14 +0100

    fixed arrow display with doors

--- store/constants.gd
+++ store/constants.gd
@@ -15,7 +15,7 @@ const HARD_SPEED = 80
 
 #muss aktualisiert werden, wenn Level eingefügt werden
 const LEVEL_PATHS = [HUB_PATH, LEVEL_1_PATH, LEVEL_2_PATH, LEVEL_3_PATH, LEVEL_4_PATH]
-const HUB_PATH = "res://scenes/level/level_2.tscn"
+const HUB_PATH = "res://scenes/level/demo.tscn"
 const LEVEL_1_PATH = "res://scenes/level/level_1.tscn"
 const LEVEL_2_PATH = "res://scenes/level/level_2.tscn"
 const LEVEL_3_PATH = "res://scenes/level/level_3.tscn"
