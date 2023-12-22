6302ed4ad7c54e1aa777fb99f3347e36964ea4b8
Benedikt <148131976+Benedikt52@users.noreply.github.com>
2023-12-22 17:45:25 +0100

    added entrance door

--- scripts/level/level_3.gd
+++ scripts/level/level_3.gd
@@ -4,22 +4,6 @@ extends Node2D
 @onready var player = $Player
 @onready var fog = $Fog
 
-@onready var skeletons = $Gegner/Skeletons
-
-func update_skeleton_speed():
-	var speed: float = 100
-	
-	match PlayerVariables.difficulty:
-		PlayerVariables.Difficulty.EASY:
-			speed = Constants.SPEED_SKELETON_EASY
-		PlayerVariables.Difficulty.MEDIUM:
-			speed = Constants.SPEED_SKELETON_MEDIUM
-		PlayerVariables.Difficulty.HARD:
-			speed = Constants.SPEED_SKELETON_HARD
-			
-	for skeleton in skeletons.get_children():
-		skeleton.speed = speed
-
 func _ready():
 	fog.init(tilemap, player)
 	Health.reset_health() #muss drin bleiben!
